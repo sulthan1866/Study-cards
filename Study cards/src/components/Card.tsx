b258b2b03c83540af1590b1358e0ea15ecfb58e8
sulthan1866 <mohammed.sulthan.2972004@gmail.com>
2024-09-30 21:31:04 +0530

    card flips now

--- Study cards/src/components/Card.tsx
+++ Study cards/src/components/Card.tsx
@@ -4,24 +4,32 @@ interface Props {
   qno: number;
   question: string;
   answer: string;
-  options: ['','','',''];
+  options: ["", "", "", ""];
   correct_option: string;
   value: boolean;
-  tester:boolean
-  length:number
+  tester: boolean;
+  length: number;
   onClick: () => void;
 }
 
-function Card({question,answer,value,onClick,qno,options,correct_option,tester,length}: Props) {
-  
+function Card({
+  question,
+  answer,
+  value,
+  onClick,
+  qno,
+  options,
+  correct_option,
+  tester,
+  length,
+}: Props) {
   const [mess, setMess] = useState<string>("");
   const [option, setOption] = useState<string>("");
   const [correction, setCorrection] = useState<string>("");
-  const [marks,setMarks] = useState<number>(0);
+  const [marks, setMarks] = useState<number>(0);
 
   const changeOption = (event: React.ChangeEvent<HTMLInputElement>) => {
     setOption(event.target.value);
-    
   };
 
   const nocurser: React.CSSProperties = {
@@ -29,6 +37,15 @@ function Card({question,answer,value,onClick,qno,options,correct_option,tester,l
     height: 450,
 
     width: 400,
+  };
+
+  const flip: React.CSSProperties = {
+    transition: "transform 0.6s",
+    transform: value ? "rotateY(360deg)" : "none",
+  };
+
+  const unflip: React.CSSProperties = {
+    transform: value ? "rotateY(360deg)" : "none",
   };
 
   document.getElementsByName("option").forEach((o) => {
@@ -40,31 +57,34 @@ function Card({question,answer,value,onClick,qno,options,correct_option,tester,l
   const check = () => {
     if (option === correct_option) {
       setCorrection("CORRECT ANSWER");
-      if(tester && !value) setMarks(marks+1)
+      if (tester && !value) setMarks(marks + 1);
     } else {
       setCorrection("WRONG ANSWER");
     }
   };
 
-  
-
-  const showResult = ()=>{
-    const messElement=document.getElementById("mess");
-    if(qno>=length && tester){
-      if(length===marks){messElement?.classList.add("text-success") ;setMess(`${marks+" / "+length} Excellent Great job :)`)}
-        else if(length/marks<2){messElement?.classList.add("text-primary");setMess(`${marks+" / "+length} Good job !`)}
-        else if(length/marks<2.5){messElement?.classList.add("text-primary");setMess(`${marks+" / "+length} Cool !`)}
-      
-      else {messElement?.classList.add("text-danger");setMess(`${marks+" / "+length} Prepare well next time !!!`);}
-      
-      
-
+  const showResult = () => {
+    const messElement = document.getElementById("mess");
+    if (qno >= length && tester) {
+      if (length === marks) {
+        messElement?.classList.add("text-success");
+        setMess(`${marks + " / " + length} Excellent Great job :)`);
+      } else if (length / marks < 2) {
+        messElement?.classList.add("text-primary");
+        setMess(`${marks + " / " + length} Good job !`);
+      } else if (length / marks < 2.5) {
+        messElement?.classList.add("text-primary");
+        setMess(`${marks + " / " + length} Cool !`);
+      } else {
+        messElement?.classList.add("text-danger");
+        setMess(`${marks + " / " + length} Prepare well next time !!!`);
+      }
     }
-  }
+  };
 
   return (
     <div
-      style={nocurser}
+      style={{ ...nocurser, ...flip }}
       className={"card bg-dark p-lg-3 p-sm-1 p-1 card-deck"}
       onDoubleClick={() => {
         onClick();
@@ -73,14 +93,14 @@ function Card({question,answer,value,onClick,qno,options,correct_option,tester,l
         showResult();
       }}
     >
-      <div className="card-body text-light" id="card">
+      <div className="card-body text-light" id="card" style={unflip}>
         <h3>
           <strong className=" card-title">
             {value ? correction : `Question: ${qno}`}
           </strong>
         </h3>
         <p className="card-text">
-          {value ? `correct option :"${correct_option}"` : question}
+          {value ? `Correct Option :"${correct_option}"` : question}
         </p>
         {value ? (
           <p className="">{`Explanation : ${answer} `}</p>
@@ -89,7 +109,6 @@ function Card({question,answer,value,onClick,qno,options,correct_option,tester,l
             <li>
               <label className="m-2">
                 <input
-                
                   type="radio"
                   value={options[0]}
                   name="option"
@@ -134,7 +153,13 @@ function Card({question,answer,value,onClick,qno,options,correct_option,tester,l
           </ul>
         )}
         <p id="mess">
-          {(tester)?(qno==1&&!value)?mess:(qno>=length)?mess:` marks = ${marks}`:mess}
+          {tester
+            ? qno == 1 && !value
+              ? mess
+              : qno >= length
+              ? mess
+              : ` marks = ${marks}`
+            : mess}
         </p>
       </div>
     </div>
