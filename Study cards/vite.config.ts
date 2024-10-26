d9588a2123274a375b7cfa7b020bf337f7a34810
Mohammed Sulthan <mohammed.sulthan.2972004@gmail.com>
2024-10-26 23:49:58 +0530

    Update vite.config.ts

--- Study cards/vite.config.ts
+++ Study cards/vite.config.ts
@@ -3,14 +3,14 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Other server configurations if needed
+  },
   build: {
     rollupOptions: {
       input: {
         main: 'index.html',
       },
     },
-  },
-  server: {
-    historyApiFallback: true,
   },
 });
